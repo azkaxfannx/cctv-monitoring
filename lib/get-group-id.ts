126b13474aeef6ea2a3e50155caf7c550f80bb67
azkaxfannx <azkamaulana6789@gmail.com>
2025-10-20 21:30:42 +0700

    better scrap

--- lib/get-group-id.ts
+++ lib/get-group-id.ts
@@ -1,33 +1,32 @@
-// lib/get-group-id.ts
+import qrcode from "qrcode-terminal";
 import { Client, LocalAuth } from "whatsapp-web.js";
-import { executablePath } from "puppeteer-core";
 
-export async function getGroupIds() {
-  const client = new Client({
-    authStrategy: new LocalAuth(),
-    puppeteer: {
-      headless: true,
-      executablePath:
-        "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
-      args: [
-        "--no-sandbox",
-        "--disable-setuid-sandbox",
-        "--disable-dev-shm-usage",
-        "--disable-accelerated-2d-canvas",
-        "--no-first-run",
-        "--no-zygote",
-        "--disable-gpu",
-        "--disable-features=site-per-process",
-        "--disable-web-security",
-        "--ignore-certificate-errors",
-        "--start-maximized",
-      ],
-    },
-  });
+const client = new Client({
+  authStrategy: new LocalAuth(),
+  puppeteer: {
+    headless: true,
+    executablePath:
+      "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-accelerated-2d-canvas",
+      "--no-first-run",
+      "--no-zygote",
+      "--disable-gpu",
+      "--disable-features=site-per-process",
+      "--disable-web-security",
+      "--ignore-certificate-errors",
+      "--start-maximized",
+    ],
+  },
+});
 
-  client.on("qr", (qr) => console.log("📱 Scan QR Code ini:\n", qr));
-  client.on("ready", () => console.log("✅ WhatsApp Bot ready!"));
-  client.initialize();
-}
+client.on("qr", (qr) => {
+  console.log("📱 Scan QR Code ini:");
+  qrcode.generate(qr, { small: true });
+});
 
-getGroupIds().catch(console.error);
+client.on("ready", () => console.log("✅ WhatsApp Bot ready!"));
+client.initialize();
